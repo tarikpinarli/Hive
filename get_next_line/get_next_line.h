9e0b438a7b8c77f8cc068efc8dbee6a9daad2165
Tarik Pinarli <tpinarli@c1r6p20.hive.fi>
2024-12-12 14:57:33 +0200

    all

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,17 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 13:07:15 by tpinarli          #+#    #+#             */
+/*   Updated: 2024/12/12 14:01:58 by tpinarli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 10
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdint.h>
 
 void	*ft_calloc(size_t elementCount, size_t elementSize);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-void    *ft_memset(void *s, int c, size_t n);
-size_t  ft_strlen(const char    *str);
+void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strlen(const char *str);
 void	ft_bzero(void *s, size_t n);
+char	*get_next_line(int fd);
+
 #endif
