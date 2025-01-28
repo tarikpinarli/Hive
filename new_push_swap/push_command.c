ac3b1a1175d1f32400c82004377ca17843bf6585
tarik pinarli <tarikpinarli@tariks-MacBook-Air.local>
2025-01-28 16:43:53 +0200

    all p_s

---
+++ new_push_swap/push_command.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_command.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: utente <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 10:36:47 by utente            #+#    #+#             */
+/*   Updated: 2023/04/02 10:20:10 by utente           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+ * Push a node to the top from src to dest
+ * 🚨 attention If stack empty (i.e. NULL)
+*/
+static void	push(t_stack_node **dest, t_stack_node **src)
+{
+	t_stack_node	*node_to_push;
+
+	if (NULL == *src)
+		return ;
+	node_to_push = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	node_to_push->prev = NULL;
+	if (NULL == *dest)
+	{
+		*dest = node_to_push;
+		node_to_push->next = NULL;
+	}
+	else
+	{
+		node_to_push->next = *dest;
+		node_to_push->next->prev = node_to_push;
+		*dest = node_to_push;
+	}
+}
+
+void	pa(t_stack_node **a, t_stack_node **b)
+{
+	push(a, b);
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_stack_node **b, t_stack_node **a)
+{
+	push(b, a);
+	write(1, "pb\n", 3);
+}
---
+++ new_push_swap/rotate_command.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_command.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: utente <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 10:07:07 by utente            #+#    #+#             */
+/*   Updated: 2023/05/15 10:38:21 by utente           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+ * Top node to bottom position
+*/
+static void	rotate(t_stack_node **stack)
+{
+	t_stack_node	*last_node;
+	int				len;
+
+	len = stack_len(*stack);
+	if (NULL == stack || NULL == *stack || 1 == len)
+		return ;
+	last_node = find_last_node(*stack);
+	last_node->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
+}	
+
+void	ra(t_stack_node **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack_node **b)
+{
+	rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack_node **a, t_stack_node **b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
+}
